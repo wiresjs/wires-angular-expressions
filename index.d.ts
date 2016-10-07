7b5b813a3f2ab1d8f5b324adfdf3c15eb902f3f3
Ivan.Orlov <ivan@morrr.com>
2016-10-07 15:32:07 +0300

    Primivite typings

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+/*
+Exports.Lexer = Lexer
+Exports.Parser = Parser;
+Exports.Compile = compile
+Exports.filters = filters;*/
+/**
+ * Watches object property
+ *
+ * @export
+ * @param {*} obj
+ * @param {string} path
+ * @param {{ (...args): any }} closure
+ */
+export function Lexer(...any)
+export function Parser(...any)
+export function Compile(...any)
+export const filters : any;
